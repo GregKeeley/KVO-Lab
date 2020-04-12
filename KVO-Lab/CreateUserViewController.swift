f99355d5b2d2116c12d73e15d3837b5bd1adef8a
Gregory Keeley <gregorykeeley@pursuit.org>
2020-04-12 17:56:14 -0400

    - Finally made a little progress, uploading changes

--- KVO-Lab/CreateUserViewController.swift
+++ KVO-Lab/CreateUserViewController.swift
@@ -12,16 +12,28 @@ class CreateUserViewController: UIViewController {
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var balanceTextField: UITextField!
     
+    private var users = [User]()
+    
+    private var userObservation: NSKeyValueObservation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
+    //TODO: this belongs in the tableview, to oberserve the balance of the user, along with the userObservation
+    private func setupUserObserver() {
+        userObservation = User.shared.observe(\.balance, options: [.new], changeHandler: { [weak self] (balance, change) in
+            guard let newBalance = change.newValue else { return }
+        })
+    }
     @IBAction func createUserButtonPressed(_ sender: UIButton) {
         guard let userName = usernameTextField.text,
             let balance = balanceTextField.text
         else { return }
-        let user = User(userName: userName, balance: balance, users: <#T##[User]#>)
+        let user = User.init()
+        user.balance = balance
+        user.userName = userName
+        users.append(user)
     }
     
 }
--- KVO-Lab/User.swift
+++ KVO-Lab/User.swift
@@ -9,20 +9,20 @@
 import Foundation
 
 @objc class User: NSObject {
-//    static var shared = User()
+    static var shared = User()
     @objc dynamic var userName: String
-    @objc dynamic var balance: Int
+    @objc dynamic var balance: String
     
-    init(userName: String, balance: Int) {
-        self.userName = userName
-        self.balance = balance
+    override public init() {
+        userName = ""
+        balance = "0"
     }
+    
+//    init(userName: String, balance: String) {
+//        self.userName = userName
+//        self.balance = balance
+//    }
 }
 
-//@objc class Users: NSObject {
-//    @objc dynamic var users = [User].self
-//    init(users: [User]) {
-//        self.users = users
-//    }
-//}
+
 
